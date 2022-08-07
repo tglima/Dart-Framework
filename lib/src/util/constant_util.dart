966eae92fc311293f926bafca7404acbf3279a2c
tglima <thg.limadesousa@gmail.com>
2022-08-07 18:02:00 -0300

    Add constant_util.dart

---
+++ lib/src/util/constant_util.dart
@@ -0,0 +1,5 @@
+class ConstantUtil {
+  static const String ok = 'OK';
+  static const String msgError = 'An error has occurred!';
+  static const String invalidKeyCrypt = 'The KeyCrypt is invalid!';
+}
